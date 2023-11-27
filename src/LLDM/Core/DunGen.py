47b877e79f520ab8a7e20a4838e938867f127915
Manchips <mansanchez@csumb.edu>
2023-11-27 15:16:21 -0800

    Added general room adding
    Rooms just get added one by one after another as long it isn't the first one i.e 1->2 2->3 & etc. I'll attempt to set it up so instead rooms have a either 2-3

--- src/LLDM/Core/DunGen.py
+++ src/LLDM/Core/DunGen.py
@@ -1,13 +1,17 @@
 from LLDM.Core.Scene import Location, Map
+import random
+import enum
+
 
 # TODO: Generate a fully-structured dungeon with notable locations and entrance/exit.
 # Create the Map. Note: the technical term for our map is a 'graph'.
 
 # Setting these up as true or false for right now, starting_room should only be set to true once, this is pretty bare bones and I'm not too sure its a good approach in the long term
-class RoomFlag(Enum):
-    starting_room = false
-    has_chest = false
-    has_npc = false
+class RoomFlag(enum):
+    starting_room = False
+    has_chest = False
+    has_npc = False
+
 
 def setup_dungeon():
     # Replace the following with a randomly made graph and adjacent nodes. (or seek sizing via user input)
@@ -18,12 +22,12 @@ def setup_dungeon():
 
     # Set this up, so we generate a dungeon with random connections
     map1 = Map()
-    roooms = []
+    rooms = []
 
     # For right now we don't have an input, so I'm just going to set it up with 5 rooms
     for i in range(5):
-        room_name = f"Room{i+1}"
-        room_description = f"The {i+1} room."
+        room_name = f"Room{i + 1}"
+        room_description = f"The {i + 1} room."
         room_flags = []
 
         # Only do this for the first room, probably a bad idea since in the future we may want a random middle room to the be starting point
@@ -35,20 +39,22 @@ def setup_dungeon():
         # For both loot and NPC right now there will be a 20% chance for loot or an npc these are not mutually exclusive so there could be both a npc and loot
         # Also updating room_description based on if the flags are appended
         if random.random() < 0.2:
-            roon_flags.append(RoomFlag.has_chest)
+            room_flags.append(RoomFlag.has_chest)
             room_description += "It has a chest."
         if random.random() < 0.2:
             room_flags.append(RoomFlag.has_npc)
             room_description += "It has an NPC inside"
 
         # Finally we create the room with the flags, I will need to add to connection flags as well
-        room[i] = Location(room_name, room_description) # I'm not even sure if we need to pass the flags since they'll be in the description
-        map1.add_location(room[i]) #Add to map, only issue is that it'll always add something just called room I'll have to ask about this
+        rooms[i] = Location(room_name,room_description)  # I'm not even sure if we need to pass the flags since they'll be in the description
+        map1.add_location(rooms[i])  # Add to map, only issue is that it'll always add something just called room I'll have to ask about this
+
+        # Currently I'll just set rooms to be connected to one another i.e 1->2->3->etc
+        if i > 0:
+            map1.connect_locations(rooms[i-1],rooms[i])
 
     # At the end we return the map1
     return map1
-
-
 
     # room1 = Location("Room 1", "The first room of a sprawling dungeon. It has a closed door off to the side.")
     # room2 = Location("Room 2", "The second room. It has a door to the first room, and another door-to an unknown area.")
